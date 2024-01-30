5fed490e06a7d5c3cdf5d85e51a39f0655c3b34d
vikram1160 <global-vgeditor12@gmail.com>
2024-01-30 02:14:29 -0800

    add alt img tag

--- src/components/CarouselImg.jsx
+++ src/components/CarouselImg.jsx
@@ -8,21 +8,21 @@ function UncontrolledExample() {
   return (
     <Carousel>
       <Carousel.Item>
-        <img src={shop1}/>
+        <img src={shop1} alt='shop1'/>
         <Carousel.Caption>
           {/* <h3>First slide label</h3> */}
           {/* <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-      <img src={shop2}/>
+      <img src={shop2} alt='shop2'/>
         <Carousel.Caption>
           {/* <h3>Second slide label</h3> */}
           {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-      <img src={shop3}/>
+      <img src={shop3} shop3/>
         <Carousel.Caption>
           {/* <h3>Third slide label</h3> */}
           <p>
