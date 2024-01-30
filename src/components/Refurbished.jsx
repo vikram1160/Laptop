e333b25a2a60c9231fe54125eb9949001ca46dbd
vikram1160 <global-vgeditor12@gmail.com>
2024-01-30 02:23:19 -0800

    30/1/2024

--- src/components/Refurbished.jsx
+++ src/components/Refurbished.jsx
@@ -5,7 +5,7 @@ import shop2 from '../assets/shop4.jpg'
 const Refurbished = () => {
   return (
     <div>
-      <img className="shoptwo" src={shop2}/>
+      <img className="shoptwo" src={shop2} alt='shop3'/>
     </div>
   )
 }
